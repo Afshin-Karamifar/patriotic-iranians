e36ae85605cc4d5a64b5243611e570f06e1e98d9
Afshin-Karamifar <afshin.karamifar@outlook.com>
2022-12-09 13:34:07 +0000

    update 02 9 dec

--- pages/Header/index.tsx
+++ pages/Header/index.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import { View } from "../../types/types";
 import IranFalg from "./Faravahar.svg";
+import QR_Code from "./QR_Code.svg";
 import styles from "./Header.module.css";
 import { RWebShare } from "react-web-share";
+import "animate.css";
+import { useState } from "react";
 
 const Header = ({ views }: { views: View }) => {
+  const [showQRCode, setShowQRCode] = useState(false);
   return (
     <div className={styles.container}>
-      <div className={styles.view}>
-        <i className="fa-solid fa-eye"></i>
-        <p className={styles.quantity}>{views?.quantity}</p>
+      <div className={styles.view_container_01}>
+        <div
+          className={styles.qr_icon_container}
+          onClick={() => setShowQRCode((prevState) => !prevState)}
+        >
+          <i className={`fa-solid fa-qrcode fa-xl ${styles.qr}`}></i>
+        </div>
+        <div className={styles.view}>
+          <i className="fa-solid fa-eye"></i>
+          <p className={styles.quantity}>{views?.quantity}</p>
+        </div>
       </div>
       <div className={styles.logo}>
         <Image
@@ -32,6 +44,23 @@ const Header = ({ views }: { views: View }) => {
           <i className={`fa-solid fa-share fa-m ${styles.share}`}></i>
         </div>
       </RWebShare>
+      {showQRCode && (
+        <div
+          className={`animate__animated animate__fadeIn ${styles.qr_code}`}
+          onClick={() => setShowQRCode((prevState) => !prevState)}
+        >
+          <div>
+            <Image
+              alt={"Women-Life-Freedom"}
+              src={QR_Code}
+              style={{ width: "300px", height: "300px" }}
+              className={styles.qr_code_main}
+              priority
+            ></Image>
+            <p className={styles.tt_close}>Tap to close.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- pages/Views/index.tsx
+++ pages/Views/index.tsx
@@ -22,39 +22,23 @@ const Views = () => {
       {showQuote && (
         <div className={`${styles.quote} animate__animated animate__fadeIn`}>
           <p>
-            <i className="fa-solid fa-quote-left"></i>&nbsp; I am Cyrus, King of
-            the World, The Great King, The Just King, King of Babylon, King of
-            Sumer and Akkad, King of the World. Son of Cambyses, the Great King
-            … When I entered Babylon without war and struggle, all the people
-            accepted my steps with joy. On the throne of the kings of Babylon, I
-            sat. I sat on the throne of Marduk, the Marduk (Babylonian God),
-            turned the pure hearts of the people of Babylon to me because I
-            honored him. My great army slowly entered Babylon. I did not allow
-            the suffering of the people of this city and this land. I abolished
-            slavery, ending their misery. … I commanded no one to overthrow the
-            inhabitants of the city. I commanded that all people should be free
-            to worship their god and that no one would need them. The Great God
-            was pleased with my deeds … He blessed us with his kindness. We all
-            praised his high position in a joyful and peaceful way … I rebuilt
-            all the cities that had been destroyed. I commanded to open all the
-            shrines that had been closed. I returned all the people who were
-            scattered and displaced to their homeland and built their ruined
-            homes so that their hearts would rejoice and that I would be
-            long-living in their prayers every day … I provided a peaceful
-            community for all people, and I gave peace to all people. I respect
-            all the traditions and religions of Babylon, Akkad, and other
-            countries under my command. All people in the lands under my command
-            are free to choose their religion, work, and place of residence. As
-            long as I am alive, no one will be allowed to take over other
-            property by force. I am not going to let anyone else do the work
-            without getting paid. No one should be punished for a crime
-            committed by his relatives. I will stop slavery of men and women,
-            and I command my government to do so until this ugly tradition is
-            removed from the earth. I will build the ruined cities beyond the
-            Tigris and their places of worship so that their inhabitants, who
-            were previously brought to Babylon as slaves, may return to their
-            homes. &nbsp;
-            <i className="fa-solid fa-quote-right"></i>
+            Protests in Iran were
+            sparked by the death of a 22-year-old Kurdish woman, Mahsa Amini,
+            known by her Kurdish name, Jina, who was arrested and violently
+            beaten by the country’s morality police. Amini died on September 16,
+            resulting in national outrage and mobilising a women-led movement
+            that has garnered international support. Protesters are calling for
+            the end of oppressive laws and the dissolution of the country’s
+            Islamic government. Currently, Iranians of all ages, ethnicities,
+            and genders have joined in the demonstrations, but it is mainly the
+            younger generations that have taken to the streets. Women and men
+            are shoulder-to-shoulder. All of Iran is united, and for the first
+            time in the history of Iran since the Islamic Revolution, there is
+            this unique unity between the ethnicities. Everyone is chanting the
+            same slogan. Their demand is the same. According to Human Rights
+            Activists in Iran (HRA), more than 475 people have been killed by
+            security forces while participating in anti-government protests,
+            and over 18,000 have been detained by authorities.
           </p>
         </div>
       )}
