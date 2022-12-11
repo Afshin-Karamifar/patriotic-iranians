3d83237df43b848048c86ca7d2f5278159f5c4ae
Afshin-Karamifar <afshin.karamifar@outlook.com>
2022-12-11 22:45:14 +0000

    6update 28 11 dec

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["patriotics.s3.eu-west-2.amazonaws.com/", "localhost"],
-    formats: ["image/avif", "image/webp"],
+    domains: ["patriotics.s3.eu-west-2.amazonaws.com", "localhost"],
+    formats: ["image/avif", "image/webp", "image/png", "image/jpeg"],
   },
 };
 
